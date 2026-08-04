9a3f31cdefd72f5cfa5f656e55d7fdc7c620d916
abdimuminovabduzafar-beep <abdimuminovabduzafar@gmail.com>
2026-08-04 22:11:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-// Intro animatsiya
-setTimeout(() => {
-    const loader = document.getElementById("loader");
-    if (loader) {
-        loader.style.display = "none";
-    }
-}, 2500);
+let count = Number(localStorage.getItem("lady_counter") || 0);
+count++;
+localStorage.setItem("lady_counter", count);
 
-// Oddiy tashrif hisoblagichi (faqat shu brauzer uchun)
-let count = localStorage.getItem("lady_views");
-
-if (!count) {
-    count = 1;
-} else {
-    count = Number(count) + 1;
-}
-
-localStorage.setItem("lady_views", count);
-
-const counter = document.getElementById("counter");
-
-if (counter) {
-    counter.innerText = count;
+const el = document.getElementById("counter");
+if (el) {
+  el.textContent = count;
 }
